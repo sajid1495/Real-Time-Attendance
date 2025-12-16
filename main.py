4a9d68cb6aa37f0ca7d22b76c22d12c2a8392dea
MD.SAJEDUL ISLAM <m.sajid1495@gmail.com>
2025-12-16 11:51:41 +0600

    Adding Buttons

--- main.py
+++ main.py
@@ -23,26 +23,92 @@ class Face_Recognition_System:
 
         #students button
         student = Image.open(r"images\students.png")  
-        student = student.resize((220, 220), Image.Resampling.LANCZOS)
+        student = student.resize((180, 180), Image.Resampling.LANCZOS)
         self.photostudent = ImageTk.PhotoImage(student)
 
         btn1 = Button(bg_image, image=self.photostudent, cursor="hand2")
-        btn1.place(x=200, y=100, width=220, height=220)
+        btn1.place(x=255, y=130, width=180, height=180)
 
-        btn1_text = Button(bg_image, text="Students Details", cursor="hand2", font=("Calibri", 16, "bold"))
-        btn1_text.place(x=200, y=320, width=220, height=40)
+        btn1_text = Button(bg_image, text="Students Details", cursor="hand2", font=("Calibri", 14, "bold"))
+        btn1_text.place(x=255, y=310, width=180, height=40)
 
 
         #detect face button
         detect_face = Image.open(r"images\detect_faces.png")  
-        detect_face = detect_face.resize((220, 220), Image.Resampling.LANCZOS)
+        detect_face = detect_face.resize((180, 180), Image.Resampling.LANCZOS)
         self.photodetect_face = ImageTk.PhotoImage(detect_face)
 
-        btn1 = Button(bg_image, image=self.photodetect_face, cursor="hand2")
-        btn1.place(x=450, y=100, width=220, height=220)
+        btn2 = Button(bg_image, image=self.photodetect_face, cursor="hand2")
+        btn2.place(x=535, y=130, width=180, height=180)
 
-        btn1_text = Button(bg_image, text="Detect Face", cursor="hand2", font=("Calibri", 16, "bold"))
-        btn1_text.place(x=450, y=320, width=220, height=40)
+        btn2_text = Button(bg_image, text="Face Detector", cursor="hand2", font=("Calibri", 14, "bold"))
+        btn2_text.place(x=535, y=310, width=180, height=40)
+
+        #attendance button
+        attendance = Image.open(r"images\attendance.png")  
+        attendance = attendance.resize((180, 180), Image.Resampling.LANCZOS)
+        self.photoattendance = ImageTk.PhotoImage(attendance)
+
+        btn3 = Button(bg_image, image=self.photoattendance, cursor="hand2")
+        btn3.place(x=815, y=130, width=180, height=180)
+
+        btn3_text = Button(bg_image, text="Attendance", cursor="hand2", font=("Calibri", 14, "bold"))
+        btn3_text.place(x=815, y=310, width=180, height=40)
+
+        #train data button
+        traindata = Image.open(r"images\train_data.png")  
+        traindata = traindata.resize((180, 180), Image.Resampling.LANCZOS)
+        self.phototraindata = ImageTk.PhotoImage(traindata)
+
+        btn4 = Button(bg_image, image=self.phototraindata, cursor="hand2")
+        btn4.place(x=1095, y=130, width=180, height=180)
+
+        btn4_text = Button(bg_image, text="Train Data", cursor="hand2", font=("Calibri", 14, "bold"))
+        btn4_text.place(x=1095, y=310, width=180, height=40)
+
+        #help desk button
+        helpdesk = Image.open(r"images\help_desk.png")   
+        helpdesk = helpdesk.resize((180, 180), Image.Resampling.LANCZOS)
+        self.photohelpdesk = ImageTk.PhotoImage(helpdesk)
+
+        btn5 = Button(bg_image, image=self.photohelpdesk, cursor="hand2")
+        btn5.place(x=255, y=420, width=180, height=180)
+
+        btn5_text = Button(bg_image, text="Help Desk", cursor="hand2", font=("Calibri", 14, "bold"))
+        btn5_text.place(x=255, y=600, width=180, height=40)
+
+        #photos button
+        photos = Image.open(r"images\photos.png")  
+        photos = photos.resize((180, 180), Image.Resampling.LANCZOS)
+        self.photophotos = ImageTk.PhotoImage(photos)
+
+        btn6 = Button(bg_image, image=self.photophotos, cursor="hand2")
+        btn6.place(x=535, y=420, width=180, height=180)
+
+        btn6_text = Button(bg_image, text="Photoes", cursor="hand2", font=("Calibri", 14, "bold"))
+        btn6_text.place(x=535, y=600, width=180, height=40)
+
+        #developer button
+        developer = Image.open(r"images\developer.png")   
+        developer = developer.resize((180, 180), Image.Resampling.LANCZOS)
+        self.photodeveloper = ImageTk.PhotoImage(developer)
+
+        btn7 = Button(bg_image, image=self.photodeveloper, cursor="hand2")
+        btn7.place(x=815, y=420, width=180, height=180)
+
+        btn7_text = Button(bg_image, text="Developer", cursor="hand2", font=("Calibri", 14, "bold"))
+        btn7_text.place(x=815, y=600, width=180, height=40)
+
+        #exit button
+        exitbtn = Image.open(r"images\exit.png")   
+        exitbtn = exitbtn.resize((180, 180), Image.Resampling.LANCZOS)
+        self.photoexit = ImageTk.PhotoImage(exitbtn)
+
+        btn8 = Button(bg_image, image=self.photoexit, cursor="hand2")
+        btn8.place(x=1095, y=420, width=180, height=180)
+
+        btn8_text = Button(bg_image, text="Exit", cursor="hand2", font=("Calibri", 14, "bold"))
+        btn8_text.place(x=1095, y=600, width=180, height=40)
         
 
 if __name__ == "__main__":
