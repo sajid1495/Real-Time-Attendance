66abc6cb73db63abebf23e17be907d735d8fdb8a
MD.SAJEDUL ISLAM <m.sajid1495@gmail.com>
2025-12-18 22:40:33 +0600

    intigrating open-CV for taking sample

--- student.py
+++ student.py
@@ -2,6 +2,7 @@ from tkinter import*
 from tkinter import ttk, messagebox
 from PIL import Image, ImageTk 
 import mysql.connector 
+import cv2
 
 
 class Student:
@@ -191,7 +192,7 @@ class Student:
         btn_frame = Frame(student_frame, bd=2, relief=RIDGE, bg="white")
         btn_frame.place(x=0, y=220, width=710, height=68)
 
-        take_photo_btn = Button(btn_frame, text="Take Photo Sample", width=43, font=("Calibri", 11, "bold"), bg="blue", fg="white")
+        take_photo_btn = Button(btn_frame, command=self.generate_dataset, text="Take Photo Sample", width=43, font=("Calibri", 11, "bold"), bg="blue", fg="white")
         take_photo_btn.grid(row=0, column=0, columnspan=2, padx=0, pady=1, sticky=W)
         update_photo_btn = Button(btn_frame, text="Update Photo Sample", width=43, font=("Calibri", 11, "bold"), bg="blue", fg="white")
         update_photo_btn.grid(row=0, column=2, columnspan=2, padx=0, pady=1, sticky=E)
@@ -458,6 +459,88 @@ class Student:
         self.var_address.set("")
         self.var_teacher.set("")
         self.var_radio1.set("Yes")
+
+
+    #========generate data set or take photo sample=======
+    def generate_dataset(self):
+        if self.var_dept.get()=="Select Department" or self.var_std_name.get()=="" or self.var_std_id.get()=="":
+            messagebox.showerror("Error", "All Fields are required", parent=self.root)
+        else:
+            try:
+                conn = mysql.connector.connect(host="localhost", username="root", password="sajid@96", database="face_recognition_attendance")
+                my_cursor = conn.cursor()
+                my_cursor.execute("select * from student")
+                myresult = my_cursor.fetchall()
+                id = 0
+                for x in myresult:
+                    id += 1
+                my_cursor.execute("update student set Dept=%s, Course=%s, Year=%s, Semester=%s, Name=%s, Division=%s, Roll=%s, Gender=%s, Dob=%s, Email=%s, Phone=%s, Address=%s, Teacher=%s, PhotoSample=%s where Student_id=%s", (
+                                                                                                                                                                                                                                    self.var_dept.get(),
+                                                                                                                                                                                                                                    self.var_course.get(),
+                                                                                                                                                                                                                                    self.var_year.get(),
+                                                                                                                                                                                                                                    self.var_semester.get(),
+                                                                                                                                                                                                                                    self.var_std_name.get(),
+                                                                                                                                                                                                                                    self.var_div.get(),
+                                                                                                                                                                                                                                    self.var_roll.get(),        
+                                                                                                                                                                                                                                    self.var_gender.get(),
+                                                                                                                                                                                                                                    self.var_dob.get(),
+                                                                                                                                                                                                                                    self.var_email.get(),
+                                                                                                                                                                                                                                    self.var_phone.get(),
+                                                                                                                                                                                                                                    self.var_address.get(),
+                                                                                                                                                                                                                                    self.var_teacher.get(),
+                                                                                                                                                                                                                                    self.var_radio1.get(),
+                                                                                                                                                                                                                                    self.var_std_id.get()==id+1
+                                                                                                                                                                                                                                ))
+                conn.commit()
+                self.fetch_data()
+                conn.close()
+                
+                # Save student ID before resetting
+                student_id = self.var_std_id.get()
+                
+                #========load predefined data on face frontals from opencv========
+                face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+                def face_cropped(img):
+                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+                    #scaling factor = 1.3
+                    #minimum neighbor = 5
+
+                    for (x, y, w, h) in faces:
+                        face_cropped = img[y:y+h, x:x+w]
+                        return face_cropped
+                cap = cv2.VideoCapture(0)
+                img_id = 0
+                while True:
+                    ret, my_frame = cap.read()
+                    if face_cropped(my_frame) is not None:
+                        img_id += 1
+                        face = cv2.resize(face_cropped(my_frame), (450, 450))
+                        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+                        file_name_path = "data/user." + str(student_id) + "." + str(img_id) + ".jpg"
+                        cv2.imwrite(file_name_path, face)
+                        cv2.putText(face, str(img_id), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 2)
+                        cv2.imshow("Cropped Face", face)
+
+                    if cv2.waitKey(1) == 13 or int(img_id) == 100:
+                        break
+                cap.release()
+                cv2.destroyAllWindows()
+                self.reset_data()
+                messagebox.showinfo("Result", "Generating data set completed!", parent=self.root)
+            except Exception as es:
+                messagebox.showerror("Error", f"Due To: {str(es)}", parent=self.root)
+            
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
